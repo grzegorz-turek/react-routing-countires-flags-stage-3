b0f71ba1dab282188a96726043193d75e797394d
Grzegorz Turek <grzegorz.s.turek@gmail.com>
2019-10-21 01:46:34 +0200

    fixed DevTools import for heroku hosting

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,8 @@
 import { createStore } from 'redux';
 import reducers from '../reducers/index';
-import DevTools from '../DevTools';
+//import DevTools from '../DevTools';// wersja z DevToolsami
 
-const store = createStore(reducers, DevTools.instrument());
+//const store = createStore(reducers, DevTools.instrument());// wersja z DevToolsami
+const store = createStore(reducers);
 
 export default store;
